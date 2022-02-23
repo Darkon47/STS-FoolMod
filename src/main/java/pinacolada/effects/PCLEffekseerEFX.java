84ba8a372feedad6e503abef7cb5be47ccdea313
SevenDayCandle <persisting_delusions@yahoo.com>
2022-02-22 20:58:39 -0800

    - Effekseer

---
+++ src/main/java/pinacolada/effects/PCLEffekseerEFX.java
@@ -0,0 +1,6 @@
+package pinacolada.effects;
+
+public class PCLEffekseerEFX {
+    public static final String SWORD1 = "effects/Sword01.efk";
+    public static final String TEST = "effects/Test.efk";
+}
